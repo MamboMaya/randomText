db121ed97d4c618e7acf22d4ab12dbf7757c1123
Damien Maya <damien_maya@yahoo.com>
2017-06-19 13:54:52 -0500

    not finished yeeeeet

--- express-app.js
+++ express-app.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const loremIpsum = require('lorem-ipsum'),
- output = loremIpsum({
-   count: 1,
- })
- console.log(loremIpsum.count);
-const num = loremIpsum.count
+const loremIpsum = require('lorem-ipsum')
+
 
 app.get('/', function(req, res) {
-  res.send('welcome, homie... go check out the lorem ipsum page!')
+  res.send('yo homie... go check out the lorem ipsum page!')
 })
 
 app.get('/lorem', function (req, res) {
+  const output = loremIpsum()
   res.send(output)
 })
-app.get(`/lorem/${num}`, function(req, res){
-  res.send(output)
-})
+
+//NOT DONE WITH HARD MODE YET!! Still working on it
+
+// app.get('/lorem/:num', function(req, res){
+//   const num = loremIpsum.count
+//   const output = loremIpsum({
+//     count: num
+//   })
+//   res.send(output)
+// })
 
 app.listen(3000, function() {
   console.log('good to go! checkout localhost:3000');
